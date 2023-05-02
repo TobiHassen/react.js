83e3cedfab06df900b87bd00995eb766615ddbd1
tobias hassen <tobiashassencoder@gmail.com>
2023-05-02 17:36:34 -0300

    Agregue una navbar al proyecto

---
+++ my-app/src/assets/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+<link rel="stylesheet" href="./NavBar.css" />
+
+const NavBar = () => {
+return (
+    <div>
+    <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+    </nav>
+    </div>
+)
+}
+
+export default NavBar
